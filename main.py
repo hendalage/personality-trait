03cccc8e57682b80db62446b6bb5c300cd433b3b
hendalage <chathushka.hendalage@student.oulu.fi>
2022-10-31 10:06:47 +0200

    data extraction

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
-import os
-import json
-import csv
-import time
+from pandas import *
 
 
-def get_counts(url, params, hash_tag):
-    return 0
+def read_data():
+    corpus = {}
+    data = read_csv("Datasets/Harry Potter 1.csv", sep=";")
+    print(data)
+    print(data['Character'].unique())
+    # data['Character'] = data['Character'].str.rstrip()
+    # print(data.Character.unique())
+    data['Character'] = data['Character'].str.replace(" ", "")
+    # data['Character'] = data['Character'].str.replace("\xa0", "")
+    print(data.Character.tolist())
+
+    # corpus['Dumbledore'] = data['Sentence'][0] + data['Sentence'][3]
+
+    for i in data.index:
+        key = data['Character'][i]
+        if key in corpus.keys():
+            corpus[key] = corpus[key] + ' ' + data['Sentence'][i]
+        else:
+            corpus[key] = data['Sentence'][i]
+    print(corpus)
+    print('\n\n')
+    print(corpus['McGonagall'])
+    corpus_df = DataFrame.from_dict(corpus, orient='index', columns=['Vocabulary'])
+
+    # print(corpus_df['Dumbledore'])
+    corpus_df.to_csv('test.csv', sep=';')
+
+    corpus['newKey'] = "value of new key"
 
 
 def main():
-    return 0
+    read_data()
 
 
 if __name__ == "__main__":
