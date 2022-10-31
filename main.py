79662170c8560dfb503da2b0b30182532e3adb6b
hendalage <chathushka.hendalage@student.oulu.fi>
2022-10-31 10:59:39 +0200

    removed comments

--- main.py
+++ main.py
@@ -3,16 +3,13 @@ from pandas import *
 
 def read_data():
     corpus = {}
+
     data = read_csv("Datasets/Harry Potter 1.csv", sep=";")
     print(data)
     print(data['Character'].unique())
-    # data['Character'] = data['Character'].str.rstrip()
-    # print(data.Character.unique())
-    data['Character'] = data['Character'].str.replace(" ", "")
-    # data['Character'] = data['Character'].str.replace("\xa0", "")
-    print(data.Character.tolist())
 
-    # corpus['Dumbledore'] = data['Sentence'][0] + data['Sentence'][3]
+    data['Character'] = data['Character'].str.replace(" ", "")
+    print(data.Character.tolist())
 
     for i in data.index:
         key = data['Character'][i]
@@ -20,15 +17,10 @@ def read_data():
             corpus[key] = corpus[key] + ' ' + data['Sentence'][i]
         else:
             corpus[key] = data['Sentence'][i]
+
     print(corpus)
-    print('\n\n')
-    print(corpus['McGonagall'])
-    corpus_df = DataFrame.from_dict(corpus, orient='index', columns=['Vocabulary'])
 
-    # print(corpus_df['Dumbledore'])
-    corpus_df.to_csv('test.csv', sep=';')
-
-    corpus['newKey'] = "value of new key"
+    # corpus_df = DataFrame.from_dict(corpus, orient='index', columns=['Vocabulary'])
 
 
 def main():
