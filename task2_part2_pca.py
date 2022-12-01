54d34c4470fb8a360107cbbfc517d8f7f3648ddb
Achira-Oulu <117048443+Achira-Oulu@users.noreply.github.com>
2022-12-01 07:47:47 +0200

    Update task2_part2_pca.py

--- task2_part2_pca.py
+++ task2_part2_pca.py
@@ -4,17 +4,17 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
-
 data = pd.read_csv("Results/harry_potter_all_aggregated_personality.csv")
-print (data)
+print(data)
 
-features = ['EXT','NEU', 'AGR' ,'CON', 'OPN']
+features = ['EXT', 'NEU', 'AGR', 'CON', 'OPN']
 
 # Separating out the features
 x = data.loc[:, features].values
 # Separating out the target
-y = data.loc[:,['Character']].values
+y = data.loc[:, ['Character']].values
 
+# testing purposes
 print(x.shape)
 
 # Standardizing the features
@@ -22,27 +22,29 @@ x_s = StandardScaler().fit_transform(x)
 print(x_s)
 print(x_s.shape)
 
+# performing PCA
 pca = PCA(n_components=2)
 principalComponents = pca.fit_transform(x_s)
-principalDf = pd.DataFrame(data = principalComponents
-             , columns = ['PC1', 'PC2'])
-
+principalDf = pd.DataFrame(data=principalComponents
+                           , columns=['PC1', 'PC2'])
+# testing purposes
 print(principalDf)
 
+# adding character names to the finalDF dataframe
 finalDf = pd.concat([data[['Character']], principalDf], axis=1)
 print(finalDf)
 
-
-# # calculating the %
+# # calculating the % variance ratio
 per_var = np.round(pca.explained_variance_ratio_ * 100)
 print(per_var)
 
-
-#plotting
+# plotting
 
 plt.scatter(finalDf.PC1, finalDf.PC2)
-plt.title('PCA Visualization')
 
+plt.ylabel('Principle component 2')
+plt.xlabel('Principle component 1')
+plt.title('PCA Visualization')
 
 for sample in finalDf.index:
     plt.annotate(sample, (finalDf.PC1.loc[sample], finalDf.PC2.loc[sample]))
